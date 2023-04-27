41b673a23f5857062c073a6bce5147d930c94eaa
Juan Francisco Fioretti <juanfrafio125@gmail.com>
2023-04-27 00:14:58 -0300

    NavBar fully added commit [ Toggle NavBar ]

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,54 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+const NavBar = () => {
+    return (
+        <>
+        {
+            [false].map((expand) => (
+                <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+                <Container fluid>
+                    <Navbar.Brand href="#">Paw's Home</Navbar.Brand>
+                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                    <Navbar.Offcanvas
+                    id={`offcanvasNavbar-expand-${expand}`}
+                    aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                    placement="end"
+                    >
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                        Paw's Home
+                        </Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="justify-content-end flex-grow-1 pe-3">
+                        <Nav.Link href="#action1">Home</Nav.Link>
+                        <Nav.Link href="#action2">Pets</Nav.Link>
+                        <NavDropdown
+                            title="Accesories"
+                            id={`offcanvasNavbarDropdown-expand-${expand}`}
+                        >
+                            <NavDropdown.Item href="#action3"> Paws </NavDropdown.Item>
+                            <NavDropdown.Item href="#action4"> Wings </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action5">
+                                About ours divitions
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                        </Nav>
+                    </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container>
+                </Navbar>)   
+                )
+            }
+        </>
+    );
+}
+
+export default NavBar;
