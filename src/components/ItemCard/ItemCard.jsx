08223da59e815a84dae065a3bc1aa4bf119784b9
Juan Francisco Fioretti <juanfrafio125@gmail.com>
2023-06-05 21:22:16 -0300

    Responsive Card CSS completed

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import styles from './styles.css';
 import { Link } from 'react-router-dom';
+import styles from './styles.css';
 
 const ItemCard = ({product}) => {
 
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,51 +1,23 @@
 import React from 'react';
 import ItemCard from '../ItemCard/ItemCard';
 import Loader from '../Loader/Loader';
+import styles from './styles.css';
 // import { useParams } from 'react-router-dom';
 // import  {getProducts}  from '../../utils/Products'
 
 const ItemList = ({ loader, products}) => {
 
-    // const [ loader, setLoader ] = useState(true);
-
-    // const [ products, setProducts ] = useState(true);
-
-    // const { categorieId } = useParams();
-
-    // useEffect(() => {
-
-    //     categorieId ? 
-    //         getProducts()
-    //         .then(response => {
-    //             setProducts(response.filter(product => product.categorie === categorieId));
-    //         })
-    //         .catch(error => console.error(error))
-    //         .finally(() => setLoader(false))
-
-    //         :
-
-    //         getProducts()
-    //         .then(response => {
-    //             setProducts(response);
-    //         })
-    //         .catch(error => console.error(error))
-    //         .finally(() => setLoader(false));
-    // }, [categorieId])
-
     return (
 
         <div id='divContainerMappingProducts'>
-            {/* <div id='divContainerProducts'> */}
-                {
-                    loader ? 
-
-                        <Loader/>
-                        :
-                        products.map((product) => 
-                            <ItemCard key={product.id} product={product}/>
-                        )
-                }
-            {/* </div> */}
+            {
+                loader ? 
+                    <Loader/>
+                    :
+                    products.map((product) => 
+                        <ItemCard key={product.id} product={product}/>
+                    )
+            }
         </div>
     )
 }
